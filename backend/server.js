cdf82ff4c7b224de5b3fcca1973030e6027c7a3d
rmallam <rmallam@users.noreply.github.com>
2025-12-12 14:27:33 +0530

    Fix CSP for RSVP invite page

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(helmet({
         directives: {
             defaultSrc: ["'self'"],
             styleSrc: ["'self'", "'unsafe-inline'"],
-            scriptSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
             imgSrc: ["'self'", "data:", "https:"],
         },
     },
