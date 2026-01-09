46ac05c007b34b312423bdefb36939eb5bcbf247
rmallam <rmallam@users.noreply.github.com>
2026-01-09 08:58:15 +0530

    fix: Syntax error in ManagerDashboard

--- frontend/src/pages/ManagerDashboard.jsx
+++ frontend/src/pages/ManagerDashboard.jsx
@@ -427,7 +427,7 @@ const ManagerDashboard = () => {
                                         </div>
                                     </Link>
                                 );
-                            }))}
+                            })}
                     </div>
                 )}
             </div>
